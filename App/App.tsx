c2b9bc669aa153b159af140027e6ff2f994f83f0
Rahul Panchal <127726141+rahulcodz@users.noreply.github.com>
2023-08-09 16:32:32 +0530

    test commit

---
+++ App/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Router> */}
+      {/* <Useroutes /> */}
+      <h1>Hello world</h1>
+    </div>
+  );
+}
+export default App;
---
+++ index.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App/Assets/styles/style.scss';
+import App from './App/App';
+// import { Provider } from 'react-redux';
+// import { store } from './App/store/store';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+    <App />
+    {/* </Provider> */}
+  </React.StrictMode>
+);
